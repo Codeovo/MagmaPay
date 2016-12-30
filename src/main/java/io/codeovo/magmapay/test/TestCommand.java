d2e9a0439057533573d97656ad303e4026569d6b
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2016-12-30 00:52:23 -0500

    Improvement to test tools to reflect API charging

--- src/main/java/io/codeovo/magmapay/test/TestCommand.java
+++ src/main/java/io/codeovo/magmapay/test/TestCommand.java
@@ -1,6 +1,8 @@
 package io.codeovo.magmapay.test;
 
 import io.codeovo.magmapay.MagmaPay;
+import io.codeovo.magmapay.objects.charges.ChargeRequest;
+import io.codeovo.magmapay.objects.charges.ChargeResponse;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,6 +16,19 @@ public class TestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         magmaPay.getPromptManager().getCreateUserManager().addPlayer((Player) commandSender);
+
+        ChargeResponse chargeResponse = MagmaPay.getMagmaPayAPI().chargePlayer(new ChargeRequest((Player) commandSender,
+                5000, "CAD", true, "Test Charge",
+                "Test Charge"));
+
+        commandSender.sendMessage(chargeResponse.getChargeId());
+        commandSender.sendMessage(chargeResponse.getFailureCode());
+        commandSender.sendMessage(chargeResponse.getFailureMessage());
+        commandSender.sendMessage(chargeResponse.getStatus());
+        commandSender.sendMessage(chargeResponse.getStripeFraudReport());
+        commandSender.sendMessage(chargeResponse.getStripeUserReport());
+        commandSender.sendMessage(String.valueOf(chargeResponse.getCreatedStamp()));
+        commandSender.sendMessage(String.valueOf(chargeResponse.isCaptured()));
 
         return false;
     }
